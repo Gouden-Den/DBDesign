87c5383bba6cac938d344e45100710ef9d4fb128
Gouden <dzx19970811@outlook.com>
2018-04-03 10:19:42 +0800

    提交

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -3,21 +3,33 @@ package service;
 import dao.UserDao;
 import entity.User;
 
+import java.sql.SQLException;
+
 public class UserService {
     private UserDao userDao = new UserDao();
 
-    public User login(String userId, String password){
-        try {
-            User user = new User();
-            user.setUserId(userId);
-            user.setPassword(password);
-            user = userDao.getUser(userId);
-            if (user != null && user.getPassword().equals(password)){
-                return user;
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+    public User login(String userId, String password) throws SQLException {
+        User user;
+        user = userDao.getUser(userId);
+        if (user != null && user.getPassword().equals(password)){
+            return user;
         }
         return null;
+    }
+
+    public boolean insert(User user) throws SQLException {
+        return userDao.insert(user);
+    }
+
+    public boolean delete(String userId) throws SQLException {
+        return userDao.delete(userId);
+    }
+
+    public boolean update(User user) throws SQLException {
+        return userDao.update(user);
+    }
+
+    public User getUser(String userId) throws SQLException {
+        return userDao.getUser(userId);
     }
 }
